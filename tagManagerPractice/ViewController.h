1562fff741065f6096adc4a3d2a966adcbff5fff
Vincent Lee <lee.m.vincent@gmail.com>
2015-02-13 10:31:57 -0800

    Initial Commit

---
+++ tagManagerPractice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tagManagerPractice
+//
+//  Created by Vincent Lee on 2/13/15.
+//  Copyright (c) 2015 VincentLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tagManagerPractice/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tagManagerPractice
+//
+//  Created by Vincent Lee on 2/13/15.
+//  Copyright (c) 2015 VincentLee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
